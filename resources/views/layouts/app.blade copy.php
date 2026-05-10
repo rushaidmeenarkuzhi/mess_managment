0743285d5c33d8fa322ecf1c5acbdf9152d2673f
Rushaid <rushaidmulla@gmail.com>
2026-05-10 18:26:03 +0300

    changes

--- resources/views/layouts/app.blade copy.php
+++ resources/views/layouts/app.blade copy.php
@@ -14,8 +14,8 @@
     <!-- Select2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 
-    {{-- <title>{{ config('app.name', 'itie') }}</title> --}}
-    <title>itie</title>
+    {{-- <title>{{ config('app.name', 'Mess Managent') }}</title> --}}
+    <title>Mess Managent</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
